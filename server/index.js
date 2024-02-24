3549946ee57c1e1cfd57906a610459efc9fe8661
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-24 15:37:51 +0700

    add cart controller

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const authRouter = require('./app/auth/router');
 const productRouter = require('./app/product/router');
 const categoryRouter = require('./app/category/router');
 const tagRouter = require('./app/tag/router');
+const deliveryAddressRouter = require('./app/deliveryAddress/router');
+const cartRouter = require('./app/cart/router');
 
 const app = express();
 
@@ -20,6 +22,8 @@ app.use('/auth', authRouter);
 app.use('/api', productRouter);
 app.use('/api', categoryRouter);
 app.use('/api', tagRouter);
+app.use('/api', deliveryAddressRouter);
+app.use('/api', cartRouter);
 
 app.get('/', (req, res) => {
   return res.send('App is listening...');
